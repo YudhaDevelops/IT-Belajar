502eecd54b160241f11a1dd38038976be40a694c
boim212 <bimaboim7@gmail.com>
2022-09-17 11:06:35 +0700

    update pertemuan 3

---
+++ Hello/src/hello/Hello.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hello;
+
+/**
+ *
+ * @author bimab
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char kel = 'a';
+        String kel1 = "a";
+                
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//        System.out.println(a);
+//        int x,y,b;
+// 
+//        b=3; //karna tidak ada deklarasi, tidak ada titik koma
+//        x=4;
+//        y=x+b; //karna a tidak di inisialisasi, aritmatika a dengan x tidak ada 
+//        System.out.println("Hasil dari persamaan y adalah :"+y);
+
+        float a;
+        a = 3.12e2F;
+        System.out.println(a);
+        
+//        System.out.println("Hello World");
+//        System.out.println("Hallo Nama Saya Yudha");
+//        
+//        System.out.print("Saya Anak Informatika");
+//        System.out.print("Saya Dari Lampung");
+//        System.out.println("Saya");
+    }
+    
+}
